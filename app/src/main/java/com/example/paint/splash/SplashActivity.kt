6a7765eda4764b018981c42e1bd6d8d7552a383f
Bruno Bacelar Abe <abe2602@gmail.com>
2021-01-24 10:54:55 -0300

    [main] Add Splash Screen

---
+++ app/src/main/java/com/example/paint/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.paint.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.example.paint.MainActivity
+import com.example.paint.R
+
+class SplashActivity : AppCompatActivity() {
+    private val splashTimeOut:Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeOut)
+    }
+}
